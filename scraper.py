d55a4b7ab05ae520317c83aef567d61fe7c9d60c
Bruno Campos <brunofdcampos@hotmail.com>
2023-08-04 13:14:51 +0300

    feat: scraper
    Signed-off-by: Bruno Campos <brunofdcampos@hotmail.com>

---
+++ scraper.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import time
+
+# Latest player ID 2509
+print("This script will scrape data from jalgpall.ee for all players with ID between the starting player ID + 500.")
+
+start_id = input("Enter the starting player ID: ")
+
+with open('player_data.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Player ID", "Name", "Total minutes played", "Birthday", "Team", "Nationality", "Total games"])
+
+    for player_id in range(int(start_id), int(start_id) + 500):
+        print(f"Looking for player with ID: {player_id}")
+        url = f"https://jalgpall.ee/voistlused/player/{player_id}/"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        try:
+            name = soup.find('p', {'class': 'player'}).text
+            total_minutes_played = soup.find('span', {'id': 'totalminutes'}).text
+            birthday = soup.find('p', text='Sünniaeg').find_next_sibling('p').text
+            team = soup.find('p', text='Klubi').find_next_sibling('p').text
+            nationality = soup.find('p', text='Kodakondsus').find_next_sibling('p').text
+            total_games = soup.find('span', {'id': 'totalgames'}).text
+
+            writer.writerow([player_id, name, total_minutes_played, birthday, team, nationality, total_games])
+            print(f"Data successfully written for player with ID: {player_id}")
+        except AttributeError:
+            # This player_id does not exist or some information is missing, skip to next player_id
+            print(f"No data found for player with ID: {player_id}")
+            continue
+        time.sleep(0.01)
+
